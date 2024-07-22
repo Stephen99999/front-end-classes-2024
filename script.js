21a1907976a7047a98fd05013ff73dba76478e86
stephen9999 <stephendivine3@gmail.com>
2024-07-22 14:23:15 +0100

    js

---
+++ script.js
@@ -0,0 +1,15 @@
+x=8;
+// if the condition is true 
+if (x==6) {
+    console.log("x is 5");
+    
+}
+// if the condition is false 
+else{
+    console.log("x isn't 5")
+}
+let a=5,b=6,c=7;
+const pi = 3.141592653;
+myname = 'johnny';
+console.log(myname)
+console.log(4++)
