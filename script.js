2cb373247660bea0a5d0eec48d68b105f730f69b
stephen9999 <stephendivine3@gmail.com>
2024-07-30 14:39:55 +0100

    from steph

--- script.js
+++ script.js
@@ -1,15 +1,67 @@
-x=8;
-// if the condition is true 
-if (x==6) {
-    console.log("x is 5");
+// let  num1 =45;
+// let  num2 = 47;
+// let num3 = 5
+// if (num1<num2 && num2>num3) {
+//     alert("true")
     
+// }
+// else{
+//     alert("false")
+// }
+
+// let day;
+// input = parseInt(prompt("enter this month day"))
+
+// switch (input) {
+//   case 0:
+//     day = "Sunday";
+//     break;
+//   case 1:
+//     day = "Monday";
+//     break;
+//   case 2:
+//     day = "Tuesday";
+//     break;
+//   case 3:
+//     day = "Wednesday";
+//     break;
+//   case 4:
+//     day = "Thursday";
+//     break;
+//   case 5:
+//     day = "Friday";
+//     break;
+//   case  6:   
+//     day = "Saturday";
+// }
+
+// alert(day)
+
+function interest(principal,rate,time){
+  let simple_interest;
+  simple_interest=(principal*rate*time)/100;
+  return simple_interest;
+  
 }
-// if the condition is false 
-else{
-    console.log("x isn't 5")
-}
-let a=5,b=6,c=7;
-const pi = 3.141592653;
-myname = 'johnny';
-console.log(myname)
-console.log(4++)
+
+add = (a,b) => {
+  return a+b;
+};
+
+// let amount,rate_percent,time_taken;
+// amount = parseInt(prompt("how much is your principal ?"));
+// rate_percent = parseInt(prompt("what is the rate of interest ?"));
+// time_taken = parseInt(prompt("how much time you have ?"));
+// value = interest(amount,rate_percent,time_taken);
+// alert(value);
+// alert(value+amount);
+
+
+ 
+let student = ["dubem",17,1.90]
+student.push("male")
+
+alert(student)
+
+let array = new array()
+Array.isArray(student);
